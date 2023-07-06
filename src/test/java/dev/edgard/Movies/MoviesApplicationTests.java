ed1fcba513c387bf801198f731765f8c0a054dfc
Edgard Gamboa <Edgard.Gamboa@imail.org>
2023-07-06 01:22:40 -0600

    clean up

---
+++ src/test/java/dev/edgard/Movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.edgard.Movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
